08dc8a1231839aef0710356265e93459fa13edf3
dustinvtran <dustinviettran@gmail.com>
2013-09-07 18:03:00 -0700

    cleaned nil.py

--- .config/awesome/widgets/now-playing-mpd.sh
+++ .config/awesome/widgets/now-playing-mpd.sh
@@ -6,9 +6,9 @@ so=$(mpc current -f %title%)
 art=$(mpc current -f %artist%)
 
 if [ "`mpc | grep "^\[paused\]"`" != "" ]; then
-    echo -n "  ⮕ | Paused: | $so | by | $art"
+    echo -n " ⮕ | Paused: | $so | by | $art "
     elif [ "`mpc | grep "^\[playing\]"`" != "" ]; then
-    echo -n "  ⮕ | Playing: | $so | by | $art"
+    echo -n " ⮕ | Playing: | $so | by | $art "
     else
-    echo -n "  ⮕ [ | mpd stopped | ]"
+    echo -n " ⮕ [ | mpd stopped | ] "
 fi
