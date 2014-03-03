dd1ed55642ab3d51168d9b4502e47047403d94d7
Dustin Tran <dustinviettran@gmail.com>
2014-03-03 04:54:34 -0800

    misc changes

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -356,7 +356,7 @@ globalkeys = awful.util.table.join(
     --awful.key({ modkey }, "a", function () run_or_raise("urxvt -name tcli -g 127x18 -e nil-transmission-remote-cli", { instance = "tcli" }) end),
     awful.key({ modkey }, "a", function () run_or_raise("urxvt -name tcli -font 'xft:uushi' -boldFont 'xft:uushi' -g 127x18 -e nil-transmission-remote-cli", { instance = "tcli" }) end),
     awful.key({ modkey }, "q", function () run_or_raise("firefox", { class = "Firefox" }) end),
-    awful.key({ modkey }, "b", function () run_or_raise("libreoffice /home/nil/Dropbox/nil/Aesthetics/Macros.ods", { instance = "VCLSalFrame" }) end),
+    awful.key({ modkey }, "b", function () run_or_raise("libreoffice /home/nil/Dropbox/nil/aesthetics/macros.ods", { instance = "VCLSalFrame" }) end),
     awful.key({ modkey }, "k", function () run_or_raise("skype",    { class = "Skype"    }) end),
 
     -- [L+E] Use this if you have both laptop and external display.
