f64d74a75aeb12a991ad279821e7c76a7fc666a6
Dustin Tran <dustinviettran@gmail.com>
2016-10-07 04:16:40 -0400

    update doc/

--- doc/setup.sh
+++ doc/setup.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-# Resources:
-#+ system with app store, easysimbl, homebrew, and caskroom
-#http://www.lowindata.com/2013/installing-scientific-python-on-mac-os-x/#numpy
-#+ python with pip and virtualenv
+#+ system with app store
+#+ homebrew, and caskroom
+#+ python, pip, virtualenv
+#  http://www.lowindata.com/2013/installing-scientific-python-on-mac-os-x/#numpy
 #  http://docs.python-guide.org/en/latest/starting/install/osx/
-#+ tex live
-#  TODO
-#+ defaults: npm, rwm
+#+ latex
+#  install mactex, then mtpro2 (local file) manually
 echo "Install all AppStore Apps at first:
   xcode
   omnifocus
@@ -19,53 +18,15 @@ echo " '\n'"
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/science
-# This list is out of date and hard to maintain. Just see the list in
-# the archives.
-brew install wget
+# see brew list
+brew install ...
 brew install ffmpeg
 
 brew install caskroom/cask/brew-cask
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-# Primary
-brew cask install dropbox
-brew cask install firefox
-brew cask install iterm2
-
-# Systems
-brew cask install asepsis
-brew cask install flash
-brew cask install karabiner
-brew cask install slate
-brew cask install easysimbl
-brew cask install java
-brew cask install seil
-brew cask install xquartz
-
-# Media
-brew cask install spotify
-brew cask install spotify-notifications
-
-# Utilities
-brew cask install hipchat
-brew cask install libreoffice
-brew cask install utorrent
-
-# Nice to have
-brew cask install atom
-brew cask install eclipse-ide
-brew cask install google-chrome
-brew cask install sublime-text
-brew cask install vlc
-
-# Development
-brew install macvim
-brew install postgresql
-brew install vim
-brew cask install mactex
-
-# Social
-brew cask install google-hangouts
+# see cask list
+brew cask install ...
 
 # cleanup
 brew cleanup
